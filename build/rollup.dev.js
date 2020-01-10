44bf5132512e804b30c037af1054311040196dcd
saxongao <258528631@qq.com>
2020-01-10 16:03:54 +0800

    chore: 配置好工程脚本

--- build/rollup.dev.js
+++ build/rollup.dev.js
@@ -1,12 +1,27 @@
-var rupphtml = require('rollup-plugin-html')
 var rollup = require('rollup')
+var resolve = require('rollup-plugin-node-resolve');
+var babel = require('rollup-plugin-babel');
 var wOptions = { 
   input: 'src/index.js',
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**',
+      babelrc: false,
+      presets: [
+        '@babel/preset-env',
+        {
+          //useBuiltIns: 'usage', 
+          // corejs: 3,
+          // modules: 'false',
+        }
+      ]
+    })
+],
   output: {
     name: 'net-state',
-    file: 'dist/anna.js',
-    format: 'cjs',
-    // format: 'iife',
+    file: 'dist/net-state.js',
+    format: 'umd',
     sourceMap: 'inline',
   },  
   watch: {
