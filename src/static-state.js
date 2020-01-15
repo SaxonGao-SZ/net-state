bf2308136c4e969b12bd403806af7bf9cc8450df
saxongao <258528631@qq.com>
2020-01-15 18:55:08 +0800

    refactor: 把 isOnLine 属性重构为方法，返回 Promise 实例。

--- src/static-state.js
+++ src/static-state.js
@@ -1,16 +1,37 @@
 import {IS_IN_WXMP} from './const';
 export default class StaticState {
     constructor (readyHandle) {
-        this._isOnLine = false;
         this._readyHandle = readyHandle;
         this._checkOnline();
     }
 
-
-    get isOnLine () {
-        return this._isOnLine;
+    isOnLine () {
+      if (IS_IN_WXMP) {
+        return this._isOnLineWX();
+      }
+      return this._isOnLineWeb();
     }
 
+    _isOnLineWeb () {
+      return new Promise((resolve, reject)=>{
+        resolve(window.navigator.onLine);
+      });
+    }
+
+    _isOnLineWX () {
+      return new Promise((resolve, reject) => {
+        wx.getNetworkType({
+          success: (res) => {
+            this._isOnLine = res.networkType === 'none' ? false : true;
+            resolve({isOnLine: this._isOnLine}); 
+          },
+          fail: (res) => {
+            this._isOnLine = false;
+            reject({isOnLine: this._isOnLine});
+          }
+        });
+      });
+    }
 
     _checkOnline () {
         if (IS_IN_WXMP) {
